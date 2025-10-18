b550b7f681f1fb27405e6644a48d3e2d60fec7a1
“david <davzy007@gmail.com>
2025-10-18 20:28:59 +0100

    removed nickname and name check on presignup

--- backend/handlers/presignup.go
+++ backend/handlers/presignup.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"backend/constants"
-	"backend/helpers"
 	"backend/utils"
 	"context"
 	"fmt"
@@ -13,25 +12,7 @@ import (
 func PreSignupHandler(ctx context.Context, event events.CognitoEventUserPoolsPreSignup) (events.CognitoEventUserPoolsPreSignup, error) {
 	// runs before user is added to cognito user pool
 
-	nickname := event.Request.UserAttributes["nickname"]
 	birthdate := event.Request.UserAttributes["birthdate"]
-	fullname := event.Request.UserAttributes["name"]
-
-	// nickname check
-	if !utils.NicknameValid(nickname) {
-		return event, fmt.Errorf("invalid nickname")
-	}
-
-	// one final check if username is still free
-	nicknameAvail, err := helpers.NewUserHelper(ctx).NicknameAvailable(nickname)
-
-	if err != nil {
-		return event, fmt.Errorf("error checking nickname availability %w", err)
-	}
-
-	if !nicknameAvail {
-		return event, fmt.Errorf("nickname taken")
-	}
 
 	// birthdate check
 	validBirthdate, err := utils.BirthdateIsValid(birthdate)
@@ -43,11 +24,5 @@ func PreSignupHandler(ctx context.Context, event events.CognitoEventUserPoolsPre
 	if !validBirthdate {
 		return event, fmt.Errorf("Birthdate is invalid, users must be between %d and %d years old, expected format is dd/mm/yyyy", constants.MIN_AGE, constants.MAX_AGE)
 	}
-
-	// fullname check
-	if !utils.NameIsValid(&fullname) {
-		return event, fmt.Errorf("Fullname cannot be longer than %d characters", constants.MAX_FULLNAME_CHARS)
-	}
-
 	return event, nil
 }
