18e6b0124fe93f9d46b4e0003ec3a13343a34b69
“david <davzy007@gmail.com>
2025-10-29 07:00:14 +0000

    debuging

--- backend/handlers/friend_request_actions.go
+++ backend/handlers/friend_request_actions.go
@@ -3,12 +3,14 @@ package handlers
 import (
 	"backend/models"
 	"context"
+	"log"
 	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 )
 
 func HandleFriendRequestActions(ctx context.Context, req *events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Printf("method is %v", req.HTTPMethod)
 	switch strings.ToLower(req.HTTPMethod) {
 	case "get":
 		return handleGetFriendRequests(ctx, req)
