9cb6a8f44509b8c7a7c8a50a7a213893fbcd5857
“david <davzy007@gmail.com>
2025-10-30 11:20:17 +0000

    fixed search bug

--- backend/handlers/search_actions.go
+++ backend/handlers/search_actions.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"backend/models"
-	"backend/utils"
 	"context"
 	"strings"
 
@@ -12,16 +11,9 @@ import (
 func HandleSearchActions(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 	switch strings.ToLower(req.RequestContext.HTTP.Method) {
 	case "get":
-		action := req.QueryStringParameters["action"]
-		if action == "nickname" {
-			return handleNicknameAvailable(ctx, req)
-		}
-
-		if utils.GetAuthUserId(req) == "" {
-			return models.UnauthorizedErrorResponse(""), nil
-		}
 		return handleSearchUser(ctx, req)
-
+	case "post":
+		return handleNicknameAvailable(ctx, req)
 	default:
 		return models.InvalidRequestErrorResponse("Invalid http method!"), nil
 	}
