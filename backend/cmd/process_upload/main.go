f825556a59b2b7f86eedaa4f47b35349bba2439d
“david <davzy007@gmail.com>
2026-04-03 14:04:07 +0100

    init process upload properly

--- backend/cmd/process_upload/main.go
+++ backend/cmd/process_upload/main.go
@@ -1,11 +1,16 @@
 package main
 
-import "backend/utils"
+import (
+	"backend/handlers"
+	"backend/utils"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
 
 func init() {
 	utils.InitHandlerDependencies(utils.WithDatabase())
 }
 
 func main() {
-
+	lambda.Start(handlers.HandleProcessUpload)
 }
