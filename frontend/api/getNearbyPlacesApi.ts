b3cf19a98877e7b2631bf1db891fca26578307fb
“david <davzy007@gmail.com>
2026-04-27 09:16:32 +0100

    bug fix in getNearbyPlacesApi

--- frontend/api/getNearbyPlacesApi.ts
+++ frontend/api/getNearbyPlacesApi.ts
@@ -1,11 +1,10 @@
-import axios, { AxiosError } from "axios"
+import axiosInstance from "@/constants/axios"
+import { AxiosError } from "axios"
 import { apiResponse, extractBackendMsg } from "./models/apiResponse"
 
 export const getNearbyPlaces = async (lat: number, lon: number, radius: number): Promise<apiResponse<string[]>> => {
   try {
-    const { data } = await axios.get<{ message: string[] }>("/api/v1/users/places", {
-      params: { lat, lon, radius }
-    })
+    const { data } = await axiosInstance.get<{ message: string[] }>(`/api/v1/users/places?lat=${lat}&lon=${lon}&radius=${radius}`)
     return { message: data.message, error: null }
   } catch (error) {
     console.log(extractBackendMsg(error))
