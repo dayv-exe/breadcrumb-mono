29c0002fd904534781ea406a072870dc74c7e86f
“david <davzy007@gmail.com>
2026-05-01 03:09:17 +0100

    big fix

--- backend/handlers/crumb_open.go
+++ backend/handlers/crumb_open.go
@@ -12,7 +12,7 @@ import (
 
 func handleOpenCrumb(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 	userID := utils.GetAuthUserId(req)
-	crumbId := strings.ToLower(req.QueryStringParameters["id"])
+	crumbId := strings.ToLower(req.PathParameters["id"])
 	sentCrumb := strings.ToLower(strings.TrimSpace(req.QueryStringParameters["sent"])) == "true"
 
 	if userID == "" {
